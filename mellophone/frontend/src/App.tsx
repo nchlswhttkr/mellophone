8bb53b0321bc93f8efcfb48deabf6b38417f88f0
Nicholas Whittaker <nchlswhttkr@gmail.com>
2019-03-31 13:46:19 +1100

    Cleanup up App component
    The application will now wait for some initial checks to take place.

--- mellophone/frontend/src/App.tsx
+++ mellophone/frontend/src/App.tsx
@@ -4,15 +4,23 @@ import Pages from "./components/pages";
 import IdentityService from "./network/IdentityService";
 import BaseRequest from "./utils/BaseRequest";
 
-class App extends React.Component {
-  async componentDidMount() {
-    await BaseRequest.get("/");
-    await IdentityService.getIdentity();
-  }
+type AppStatus = "pending" | "resolved" | "rejected";
 
-  render() {
-    return <Pages />;
-  }
+function App() {
+  const [status, setStatus] = React.useState<AppStatus>("pending");
+
+  React.useEffect(() => {
+    Promise.all([BaseRequest.get("/"), IdentityService.getIdentity()])
+      .then(() => setStatus("resolved"))
+      .catch(() => setStatus("rejected"));
+  }, []);
+
+  if (status === "rejected")
+    return <p>Something went wrong while starting Mellophone</p>;
+
+  if (status !== "pending") return <Pages />;
+
+  return null;
 }
 
 export default App;
