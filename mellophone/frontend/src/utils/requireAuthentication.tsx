eaa6f29d8f517fb798ae015e414d127531189342
Nicholas Whittaker <nchlswhttkr@gmail.com>
2019-07-02 20:56:46 +1000

    Remove accidentally staged code

--- mellophone/frontend/src/utils/requireAuthentication.tsx
+++ mellophone/frontend/src/utils/requireAuthentication.tsx
@@ -39,24 +39,3 @@ function requireAuthentication<
 }
 
 export default requireAuthentication;
-
-/**
- * Provides a shortcut to silencing the warning given when no user is authenticated in requireAuthentication().
- *
- * Returns a method which can be called to cleanup after the applicable tests have been run.
- */
-export const silenceAuthWarnings = () => {
-  if (process.env.NODE_ENV !== "test") {
-    throw new Error("silenceAuthWarnings() is only for use in testing.");
-  }
-
-  const originalWarn = console.warn;
-  console.warn = (...args: any[]) => {
-    if (/No user in sessionStore, no content rendered./.test(args[0])) return;
-    originalWarn.call(console, ...args);
-  };
-
-  return () => {
-    console.warn = originalWarn;
-  };
-};
