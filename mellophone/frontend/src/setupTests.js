69ccdcd1fe545772ec4629f151f0bcf38a4894ab
Nicholas Whittaker <nchlswhttkr@gmail.com>
2019-08-20 15:54:25 +1000

    Bump dependencies, revent async act() workaround

--- mellophone/frontend/src/setupTests.js
+++ mellophone/frontend/src/setupTests.js
@@ -19,21 +19,3 @@ afterEach(() => {
   navigate("/");
   localStorage.clear();
 });
-
-// https://github.com/testing-library/react-testing-library/issues/281#issuecomment-480349256
-
-// this is just a little hack to silence a warning that we'll get until react
-// fixes this: https://github.com/facebook/react/pull/14853
-const originalError = console.error;
-beforeAll(() => {
-  console.error = (...args) => {
-    if (/Warning.*not wrapped in act/.test(args[0])) {
-      return;
-    }
-    originalError.call(console, ...args);
-  };
-});
-
-afterAll(() => {
-  console.error = originalError;
-});
