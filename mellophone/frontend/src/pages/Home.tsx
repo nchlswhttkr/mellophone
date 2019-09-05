6f0038b192a8c586e480f4ffdc81f860bef04371
Nicholas Whittaker <nchlswhttkr@gmail.com>
2019-09-05 14:05:05 +1000

    Revise landing page styling to use CSS modules
    Also fixes CSS bugs like a boss

--- mellophone/frontend/src/pages/Home.tsx
+++ mellophone/frontend/src/pages/Home.tsx
@@ -8,7 +8,6 @@ import requireAuthentication from "../utils/requireAuthentication";
 import { useNetwork } from "../network";
 import { route } from "../utils/routing";
 import classes from "./Home.module.css";
-import "../animate.css";
 import { loadTeamsThunk } from "../ducks/teams";
 import { ITeam } from "../types";
 import { AppState } from "../ducks";
@@ -36,37 +35,11 @@ function SplashPage() {
   return (
     <Main className={classes.splash}>
       <div className={classes.cards}>
-        <div
-          style={{
-            marginTop: "-10vh",
-            top: "8vh",
-            left: "-12vw",
-            transform: "rotate(20deg)",
-            animation: "mergeCard1 1.4s ease 0.8s forwards",
-          }}>
-          Meetings
-        </div>
-        <div
-          style={{
-            top: "-8vh",
-            left: "6vw",
-            transform: "rotate(-10deg)",
-            animation: "mergeCard2 1.1s ease 1.1s forwards",
-          }}>
-          Members
-        </div>
-        <div
-          style={{
-            marginBottom: "-10vh",
-            top: "-12vh",
-            left: "-4vw",
-            transform: "rotate(40deg)",
-            animation: "mergeCard3 1s ease 1.2s forwards",
-          }}>
-          Events
-        </div>
+        <div>Meetings</div>
+        <div>Members</div>
+        <div>Events</div>
       </div>
-      <div style={{ flex: 4, minWidth: 300, padding: "8px 16px" }}>
+      <div className={classes.callout}>
         <h1>Mellophone is an app for teams</h1>
         <h2>Take meeting minutes and coordinate with your members today!</h2>
         <h2 style={{ color: "#cd2d79" }}>
