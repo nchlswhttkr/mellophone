25660834225d4a6c6d4c04e7e22a3b2d8d9e0384
Nicholas Whittaker <nchlswhttkr@gmail.com>
2019-11-02 00:48:11 +1100

    Do not download build artifacts in pipeline
    GitLab seems to only expose build artifacts after a pipeline has
    completed successfully. Instead rely on the artifacts to be passed to
    later jobs.
    
    This still satisfies the goal of each pipeline using its own version of
    the latest FE build.

--- scripts/get-frontend-build.sh
+++ scripts/get-frontend-build.sh
@@ -2,13 +2,7 @@
 
 set -e
 
-if [ -z "$1" ]; then
-    export BRANCH_TO_DOWNLOAD="master"
-else
-    export BRANCH_TO_DOWNLOAD="$1"
-fi
-
-curl -L "https://gitlab.com/nchlswhttkr/mellophone/builds/artifacts/$BRANCH_TO_DOWNLOAD/download?job=FE+Install/Build" > build.zip
+curl -L "https://gitlab.com/nchlswhttkr/mellophone/builds/artifacts/master/download?job=FE+Install/Build" > build.zip
 unzip -uo build.zip
 rm build.zip
 pipenv run python mellophone/manage.py collectstatic --clear --no-input --no-post-process
