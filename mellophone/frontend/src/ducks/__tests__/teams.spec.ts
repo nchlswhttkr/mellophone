9d387ec4c86595bd787769943d495777db54624a
Nicholas Whittaker <nchlswhttkr@gmail.com>
2019-11-01 22:47:36 +1100

    Add test for appendTeams() action

--- mellophone/frontend/src/ducks/__tests__/teams.spec.ts
+++ mellophone/frontend/src/ducks/__tests__/teams.spec.ts
@@ -19,6 +19,16 @@ it("Reacts to the session being cleared", () => {
   expect(store.getState().teams).toHaveLength(0);
 });
 
+it("Allows teams to appended", () => {
+  const team = mock.team();
+  const store = createStore(reducer);
+
+  store.dispatch(appendTeams([team]));
+
+  expect(store.getState().teams).toHaveLength(1);
+  expect(store.getState().teams[0]).toEqual(team);
+});
+
 it("Does not append duplicate teams", () => {
   const team = mock.team();
   const store = createStore(reducer, {
