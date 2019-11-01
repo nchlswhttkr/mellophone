a1d52c50e2b5f59dd8ff4059a49be500899242eb
Nicholas Whittaker <nchlswhttkr@gmail.com>
2019-11-01 23:08:01 +1100

    Use current branch's FE build in E2E tests

--- scripts/get-frontend-build.sh
+++ scripts/get-frontend-build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-curl -L "https://gitlab.com/nchlswhttkr/mellophone/builds/artifacts/master/download?job=FE+Install/Build" > build.zip
+set -e
+
+if [ -z "$1" ]; then
+    export BRANCH_TO_DOWNLOAD="master"
+else
+    export BRANCH_TO_DOWNLOAD="$1"
+fi
+
+curl -L "https://gitlab.com/nchlswhttkr/mellophone/builds/artifacts/$BRANCH_TO_DOWNLOAD/download?job=FE+Install/Build" > build.zip
 unzip -uo build.zip
 rm build.zip
 pipenv run python mellophone/manage.py collectstatic --clear --no-input --no-post-process
